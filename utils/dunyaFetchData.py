d807690b347d26155b76d35a4213ec6941bb4a4b
sankalpg <sankalp.gulati@gmail.com>
2014-02-08 23:28:14 +0100

    Added file to fetch dunya data (pitch + tonic)

---
+++ utils/dunyaFetchData.py
@@ -0,0 +1,38 @@
+import os, sys, shutil
+import numpy as np
+from mutagen import easyid3
+import requests
+
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../batchProcessing'))
+
+import batchProcessing as BP
+
+
+def obtainPitchTonicInDB(root_dir):
+
+    audiofiles = BP.GetFileNamesInDir(root_dir,'mp3')
+
+    for audiofile in audiofiles:
+        fname, ext = os.path.splitext(audiofile)
+        
+        pithcFile = fname + '.pitch'
+        tonicFile = fname + '.tonic'
+
+        mbid = easyid3.ID3(audiofile)['UFID:http://musicbrainz.org'].data
+
+        pitchUrl = 'http://dunya.compmusic.upf.edu/document/by-id/' + mbid + '/pitch?v=0.4&subtype=pitch'
+        tonicUrl = 'http://dunya.compmusic.upf.edu/document/by-id/' + mbid + '/ctonic?v=0.2&subtype=tonic'
+
+        pitchData = np.array(eval(requests.get(pitchUrl).content))
+        tonicData = float(requests.get(tonicUrl).content)
+
+        np.savetxt(pithcFile, pitchData, delimiter = "\t", fmt='%0.2f')
+        np.savetxt(tonicFile, np.array([tonicData]), delimiter = "\t" , fmt='%0.2f')
+
+
+
+
+
+
