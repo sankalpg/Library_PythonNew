3d42dbd453fc42710df89873f10f98bbf1cf8159
Sankalp Gulati <sankalp.gulati@upf.edu>
2014-02-08 23:51:38 +0100

    Added exception handling for files not in dunya

--- utils/dunyaFetchData.py
+++ utils/dunyaFetchData.py
@@ -12,26 +12,29 @@ import batchProcessing as BP
 
 def obtainPitchTonicInDB(root_dir):
 
-    audiofiles = BP.GetFileNamesInDir(root_dir,'mp3')
-
-    for audiofile in audiofiles:
-        fname, ext = os.path.splitext(audiofile)
+    	audiofiles = BP.GetFileNamesInDir(root_dir,'mp3')
+	cnt=0
+    	for audiofile in audiofiles:
+        	fname, ext = os.path.splitext(audiofile)
         
-        pithcFile = fname + '.pitch'
-        tonicFile = fname + '.tonic'
+        	pithcFile = fname + '.pitch'
+        	tonicFile = fname + '.tonic'
 
-        mbid = easyid3.ID3(audiofile)['UFID:http://musicbrainz.org'].data
+        	mbid = easyid3.ID3(audiofile)['UFID:http://musicbrainz.org'].data
 
-        pitchUrl = 'http://dunya.compmusic.upf.edu/document/by-id/' + mbid + '/pitch?v=0.4&subtype=pitch'
-        tonicUrl = 'http://dunya.compmusic.upf.edu/document/by-id/' + mbid + '/ctonic?v=0.2&subtype=tonic'
+        	pitchUrl = 'http://dunya.compmusic.upf.edu/document/by-id/' + mbid + '/pitch?v=0.4&subtype=pitch'
+        	tonicUrl = 'http://dunya.compmusic.upf.edu/document/by-id/' + mbid + '/ctonic?v=0.2&subtype=tonic'
 
-        pitchData = np.array(eval(requests.get(pitchUrl).content))
-        tonicData = float(requests.get(tonicUrl).content)
+        	try:
+			pitchData = np.array(eval(requests.get(pitchUrl).content))
+        		tonicData = float(requests.get(tonicUrl).content)
+			np.savetxt(pithcFile, pitchData, delimiter = "\t", fmt='%0.2f')
+        		np.savetxt(tonicFile, np.array([tonicData]), delimiter = "\t" , fmt='%0.2f')
+		except:
+			cnt=cnt+1
+			print audiofile
 
-        np.savetxt(pithcFile, pitchData, delimiter = "\t", fmt='%0.2f')
-        np.savetxt(tonicFile, np.array([tonicData]), delimiter = "\t" , fmt='%0.2f')
-
-
+	print cnt
 
 
 
