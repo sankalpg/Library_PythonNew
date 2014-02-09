1696628e57ec1c13ea28d109493d9859a8646c10
sankalpg <sankalp.gulati@gmail.com>
2014-02-09 02:40:03 +0100

    Handling more exceptions
    Some files do not have MBIDs embeded and some of them are not there in dunya dataset as a result sometimes the code crashed. Fixed.

--- utils/dunyaFetchData.py
+++ utils/dunyaFetchData.py
@@ -12,29 +12,29 @@ import batchProcessing as BP
 
 def obtainPitchTonicInDB(root_dir):
 
-    	audiofiles = BP.GetFileNamesInDir(root_dir,'mp3')
-	cnt=0
-    	for audiofile in audiofiles:
-        	fname, ext = os.path.splitext(audiofile)
-        
-        	pithcFile = fname + '.pitch'
-        	tonicFile = fname + '.tonic'
+        audiofiles = BP.GetFileNamesInDir(root_dir,'mp3')
+        cnt=0
+        for audiofile in audiofiles:
+            fname, ext = os.path.splitext(audiofile)
+            
+            pithcFile = fname + '.pitch'
+            tonicFile = fname + '.tonic'
 
-        	mbid = easyid3.ID3(audiofile)['UFID:http://musicbrainz.org'].data
+            if not os.path.exists(pithcFile):
+                
+                try:
+                    mbid = easyid3.ID3(audiofile)['UFID:http://musicbrainz.org'].data
+                    pitchUrl = 'http://dunya.compmusic.upf.edu/document/by-id/' + mbid + '/pitch?v=0.4&subtype=pitch'
+                    tonicUrl = 'http://dunya.compmusic.upf.edu/document/by-id/' + mbid + '/ctonic?v=0.2&subtype=tonic'
+                    pitchData = np.array(eval(requests.get(pitchUrl).content))
+                    tonicData = float(requests.get(tonicUrl).content)
+                    np.savetxt(pithcFile, pitchData, delimiter = "\t", fmt='%0.2f')
+                    np.savetxt(tonicFile, np.array([tonicData]), delimiter = "\t" , fmt='%0.2f')
+                except:
+                    cnt=cnt+1
+                    print audiofile
 
-        	pitchUrl = 'http://dunya.compmusic.upf.edu/document/by-id/' + mbid + '/pitch?v=0.4&subtype=pitch'
-        	tonicUrl = 'http://dunya.compmusic.upf.edu/document/by-id/' + mbid + '/ctonic?v=0.2&subtype=tonic'
-
-        	try:
-			pitchData = np.array(eval(requests.get(pitchUrl).content))
-        		tonicData = float(requests.get(tonicUrl).content)
-			np.savetxt(pithcFile, pitchData, delimiter = "\t", fmt='%0.2f')
-        		np.savetxt(tonicFile, np.array([tonicData]), delimiter = "\t" , fmt='%0.2f')
-		except:
-			cnt=cnt+1
-			print audiofile
-
-	print cnt
+        print cnt
 
 
 
