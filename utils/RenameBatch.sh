07649ec8b89970cc4ca9177741343d8c1bef13db
Sankalp Gulati <sankalp.gulati@upf.edu>
2014-02-13 11:56:12 +0100

    Updates a util file. trivial changes

--- utils/RenameBatch.sh
+++ utils/RenameBatch.sh
@@ -3,13 +3,13 @@ files=()
 
 while read -r -d $'\0'; do
     files+=("$REPLY")
-done < <(find $1 -iname "*.pitch" -print0)
+done < <(find $1 -iname "*.2s25motif" -print0)
 
 
 for (( i = 0 ; i  < ${#files[@]}; i++ ))
 do
         #echo ${files[$i]%.*}
-        mv ${files[$i]%.*}3sproclog  ${files[$i]%.*}.3s25proclog
+        mv ${files[$i]%.*}3sproclog  ${files[$i]%.*}.3sproclog
         mv ${files[$i]%.*}.proclog  ${files[$i]%.*}.2s25proclog
 done
 
