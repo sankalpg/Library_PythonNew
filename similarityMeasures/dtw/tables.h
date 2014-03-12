ba497af44d6b7014ae9ffe741f053f9f1c7c1d3f
Sankalp Gulati <sankalp.gulati@upf.edu>
2014-03-12 22:25:38 +0100

    Added tables for lookup of similarity measure

---
+++ similarityMeasures/dtw/tables.h
@@ -0,0 +1,14 @@
+
+#ifndef TABLES_H 
+
+#define TABLES_H
+
+
+
+#define SIM_TB_SIZE 60001
+
+//extern const float customDist_25_100___1_25[TB_SIZE];
+extern const float simShifCB[SIM_TB_SIZE];
+extern const float simShifCBExp[SIM_TB_SIZE];
+
+#endif //TABLES_H
