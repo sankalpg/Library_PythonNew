e2acf2a8ebac406c85151b08d5f14977671db9d8
Sankalp Gulati <sankalp.gulati@upf.edu>
2014-06-26 13:02:18 +0200

    Modularized network generation code

--- networkAnalysis/networkBuild/constructNetwork.cpp
+++ networkAnalysis/networkBuild/constructNetwork.cpp
@@ -1,6 +1,74 @@
 
 #include "Snap.h"
 
+static float  min_dist=1000000000, max_dist=-100000000000;
+  
+int createPatternGraphPerFile(char *fileName, PUNGraph Graph, double dist_min, double dist_max)
+{
+	FILE *fp2;
+	long long int ind1, ind2;
+	int ii=0;
+	float dist;
+
+	fp2 = fopen(fileName, "r");
+        if (fp2==NULL)
+        {
+            printf("Error opening file %s\n", fileName);
+        }
+        else
+        {
+            while(fscanf(fp2, "%lld\t%lld\t%f\n",&ind1,&ind2, &dist)!=EOF)
+            {
+                if ((dist >= dist_max)||(dist <= dist_min))
+                {
+                    continue;    
+                }
+                if (!Graph->IsNode(ind1)) Graph->AddNode(ind1);
+                if (!Graph->IsNode(ind2)) Graph->AddNode(ind2);
+                if (!Graph->IsEdge(ind1, ind2)) Graph->AddEdge(ind1, ind2);
+
+            }
+            fclose(fp2);
+        }
+  
+}
+
+int createPatternGraphPerCollection(char *listFile, char *patternKNNExt, PUNGraph Graph, double dist_min, double dist_max)
+{
+    FILE *fp1;
+    int ii=0;
+    char  tempFilename[400]= {'\0'},  patternKNNFile[400]= {'\0'};
+
+    fp1 = fopen(listFile, "r");
+	if (fp1==NULL)
+    {
+        printf("Error opening file %s\n", listFile);
+        return 0;
+    }
+        
+    while(fgets(tempFilename, 400, fp1))
+    {
+        sscanf(tempFilename, "%[^\n]s\n", patternKNNFile);
+        
+        strcat(patternKNNFile,patternKNNExt);
+        
+        printf("processing file %d\t%s\n", ii+1,patternKNNFile);
+	    createPatternGraphPerFile(patternKNNFile, Graph, dist_min, dist_max);
+        memset(tempFilename, '\0', sizeof(char)*400);
+        memset(patternKNNFile, '\0', sizeof(char)*400);
+        ii++;
+    }
+    fclose(fp1);
+    
+ 
+}
+
+
+int computeClusterCoffCurve(char *listFile, char *patternKNNExt, , char *ClusterCoffFile)
+{
+
+}
+
 int main(int argc, char* argv[])
 {
     FILE *fp1, *fp2;
@@ -8,7 +76,7 @@ int main(int argc, char* argv[])
     int verbos;
     int ii=0, jj=0;
     long long int ind1, ind2;
-    float dist, min_dist=1000000000, max_dist=-100000000000;
+    float dist;
     double clusterCoff;
     
     PUNGraph Graph = TUNGraph::New();
@@ -25,58 +93,10 @@ int main(int argc, char* argv[])
     outputNetworkFile = argv[3];     
     if( argc == 5 ){verbos = atoi(argv[4]);}
     
-    
-    
-    fp1 = fopen(listFile, "r");
-    if (fp1==NULL)
-    {
-        printf("Error opening file %s\n", listFile);
-        return 0;
-    }
-        
-    while(fgets(tempFilename, 400, fp1))
-    {
-        sscanf(tempFilename, "%[^\n]s\n", patternKNNFile);
-        
-        strcat(patternKNNFile,patternKNNExt);
-        
-        fp2 = fopen(patternKNNFile,"r");
-        if (fp2==NULL)
-        {
-            printf("Error opening file %s\n", patternKNNFile);
-        }
-        else
-        {
-            printf("processing file %d\t%s\n", ii+1, patternKNNFile);
-            while(fscanf(fp2, "%lld\t%lld\t%f\n",&ind1,&ind2, &dist)!=EOF)
-            {
-                if (!Graph->IsNode(ind1)) Graph->AddNode(ind1);
-                if (!Graph->IsNode(ind2)) Graph->AddNode(ind2);
-                if (!Graph->IsEdge(ind1, ind2)) Graph->AddEdge(ind1, ind2);
-                
-                if (min_dist > dist)
-                {
-                    min_dist = dist;
-                }
-                if (max_dist < dist)
-                {
-                    max_dist = dist;
-                }
-            }
-            
-            
-            jj++;
-            fclose(fp2);
-        }
-        
-        
-        memset(tempFilename, '\0', sizeof(char)*400);
-        memset(patternKNNFile, '\0', sizeof(char)*400);
-        ii++;
-    }
-    fclose(fp1);
-    
-    //clusterCoff = TSnap::GetClustCf(Graph);
+   
+   createPatternGraphPerCollection(listFile, patternKNNExt, Graph, 0,10000);
+
+    clusterCoff = TSnap::GetClustCf(Graph);
     
     printf("Clustering coff is %f\n",clusterCoff);
     printf("minimum and maximum distances are %f and %f respectively", min_dist, max_dist);
@@ -91,6 +111,6 @@ int main(int argc, char* argv[])
 	
 	//TSnap::SaveEdgeList(Graph, "test.txt", "Save as tab-separated list of edges");
     
-    printf("Total number of files scanned = %d and correct ones are %d\n", ii, jj);
+    //printf("Total number of files scanned = %d and correct ones are %d\n", ii, jj);
 
 }
