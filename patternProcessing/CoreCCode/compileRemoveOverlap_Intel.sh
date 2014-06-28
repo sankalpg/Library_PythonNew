aa8475f4114c4d6beacfac6f894925d932969e8d
Sankalp Gulati <sankalp.gulati@upf.edu>
2014-06-28 13:43:14 +0200

    Added compilation file

---
+++ patternProcessing/CoreCCode/compileRemoveOverlap_Intel.sh
@@ -0,0 +1,1 @@
+g++ -I. ../../similarityMeasures/dtw/dtw.c ../../basicDSPFuncs/basicDSPCFuncs.c ../../similarityMeasures/dtw/tables.c  patternProTable.c MotifDataIO.c removeOverlap.cpp  -O3 -o RemoveOverlap_O3
