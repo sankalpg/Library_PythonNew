142577b21bed8c6d4de90e1d96bb4ae96944db96
Sankalp Gulati <sankalp.gulati@upf.edu>
2014-07-08 13:13:18 +0200

    Clustering cofficient computation

--- networkAnalysis/networkBuild/constructNetwork.cpp
+++ networkAnalysis/networkBuild/constructNetwork.cpp
@@ -2,6 +2,62 @@
 #include "Snap.h"
 
 static float  min_dist=1000000000, max_dist=-100000000000;
+
+float ThshldArray[50] = {
+
+0,
+80,
+180,
+320,
+500,
+720,
+980,
+1280,
+1620,
+2000,
+2420,
+2880,
+3380,
+3920,
+4500,
+5120,
+5780,
+6480,
+7220,
+8000,
+8820,
+9680,
+10580,
+11520,
+12500,
+13520,
+14580,
+15680,
+16820,
+18000,
+19220,
+20480,
+21780,
+23120,
+24500,
+25920,
+27380,
+28880,
+30420,
+32000,
+33620,
+35280,
+36980,
+38720,
+40500,
+42320,
+44180,
+46080,
+48020,
+50001  
+};
+
+
   
 int createPatternGraphPerFile(char *fileName, PUNGraph Graph, double dist_min, double dist_max)
 {
@@ -64,25 +120,26 @@ int createPatternGraphPerCollection(char *listFile, char *patternKNNExt, PUNGrap
 }
 
 
-int computeClusterCoffCurve(char *listFile, char *patternKNNExt, , char *ClusterCoffFile)
+/*int computeClusterCoffCurve(char *listFile, char *patternKNNExt, , char *ClusterCoffFile)
 {
 
-}
+}*/
 
 int main(int argc, char* argv[])
 {
     FILE *fp1, *fp2;
-    char *listFile, *patternKNNExt,  tempFilename[400]= {'\0'}, *outputNetworkFile, patternKNNFile[400]= {'\0'};
+    char *listFile, *patternKNNExt,  tempFilename[400]= {'\0'}, *outputNetworkFile, patternKNNFile[400]= {'\0'}, outFileName[400]= {'\0'};
     int verbos;
     int ii=0, jj=0;
     long long int ind1, ind2;
     float dist;
     double clusterCoff;
+    int thresholdBin;
     
     PUNGraph Graph = TUNGraph::New();
     
     
-    if(argc < 4 || argc > 5)
+    if(argc < 5 || argc > 6)
     {
         printf("\nInvalid number of arguments!!!\n");
         exit(1);
@@ -90,16 +147,22 @@ int main(int argc, char* argv[])
     
     patternKNNExt = argv[1];
     listFile = argv[2];
-    outputNetworkFile = argv[3];     
-    if( argc == 5 ){verbos = atoi(argv[4]);}
+    outputNetworkFile = argv[3];
+    thresholdBin = atoi(argv[4]);
+    if( argc == 6 ){verbos = atoi(argv[5]);}
     
    
-   createPatternGraphPerCollection(listFile, patternKNNExt, Graph, 0,10000);
+   createPatternGraphPerCollection(listFile, patternKNNExt, Graph, 0, ThshldArray[thresholdBin-1]);
 
     clusterCoff = TSnap::GetClustCf(Graph);
     
-    printf("Clustering coff is %f\n",clusterCoff);
-    printf("minimum and maximum distances are %f and %f respectively", min_dist, max_dist);
+    sprintf(outFileName, "%d_ClusteringCoff.txt", thresholdBin);
+    fp2 = fopen(outFileName, "w");
+    fprintf(fp2, "%f\n", clusterCoff);
+    fclose(fp2);
+    
+    //printf("Clustering coff is %f\n",clusterCoff);
+    //printf("minimum and maximum distances are %f and %f respectively", min_dist, max_dist);
 
     /*TFOut FOut(outputNetworkFile); 
     Graph->Save(FOut);*/
@@ -107,7 +170,7 @@ int main(int argc, char* argv[])
     
     
     //TSnap::SaveEdgeList(Graph, outputNetworkFile, "Save as tab-separated list of edges");
-    TSnap::SavePajek(Graph, outputNetworkFile); 
+    //TSnap::SavePajek(Graph, outputNetworkFile); 
 	
 	//TSnap::SaveEdgeList(Graph, "test.txt", "Save as tab-separated list of edges");
     
