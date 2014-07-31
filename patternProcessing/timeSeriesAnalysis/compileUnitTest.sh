0f234e351351b2623d1afebbedf11e083b2a6ff1
Sankalp Gulati <sankalp.gulati@upf.edu>
2014-07-31 10:41:18 +0200

    Add files needed for unit tests

---
+++ patternProcessing/timeSeriesAnalysis/compileUnitTest.sh
@@ -0,0 +1,1 @@
+g++ -I. ../../similarityMeasures/dtw/dtw.c ../../basicDSPFuncs/basicDSPCFuncs.c ../../similarityMeasures/dtw/tables.c TSATables.cpp TSApool.cpp TSAsimilarity.cpp TSAdataIO.cpp unitTests.cpp TSAlogs.cpp -O3 -o unitTests_O3
