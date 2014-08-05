bcf778768602b5bf04c664454132328501c6980d
sankalp <sankalp.gulati@gmail.com>
2014-08-05 11:32:48 +0200

    Add new files

---
+++ patternProcessing/timeSeriesAnalysis/compileDiscovery_BF_Intel.sh
@@ -0,0 +1,1 @@
+g++ -I. ../../similarityMeasures/dtw/dtw.c ../../basicDSPFuncs/basicDSPCFuncs.c ../../similarityMeasures/dtw/tables.c TSATables.cpp TSApool.cpp TSAsimilarity.cpp TSAdataIO.cpp discoverPatterns_BF.cpp TSAlogs.cpp -O3 -o discoverPatterns_BF_O3
---
+++ patternProcessing/timeSeriesAnalysis/compileSearch_Intel.sh
@@ -0,0 +1,1 @@
+g++ -I. ../../similarityMeasures/dtw/dtw.c ../../basicDSPFuncs/basicDSPCFuncs.c ../../similarityMeasures/dtw/tables.c TSATables.cpp TSApool.cpp TSAsimilarity.cpp TSAdataIO.cpp searchPatterns.cpp TSAlogs.cpp -O3 -o searchPatterns_O3
