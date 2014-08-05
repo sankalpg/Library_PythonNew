8e970d644fc091e317ffd40378d80f2f3f4411a3
sankalp <sankalp.gulati@gmail.com>
2014-08-05 11:34:09 +0200

    Add new files

---
+++ patternProcessing/timeSeriesAnalysis/discoverPatterns_BF.cpp
@@ -0,0 +1,111 @@
+
+#include "TSAdataIO.h"
+#include "TSAsimilarity.h"
+#include "TSApool.h"
+#include "TSAlogs.h"
+
+
+using namespace std;
+
+
+int main( int argc , char *argv[])
+{
+    procParams_t *myProcParamsPtr;
+    fileExts_t *myFileExtsPtr;
+    int Err=0;
+    int verbos;
+    
+    TSAparamHandle paramHand;
+    TSAlogs logs;
+    
+    //checking if the number of input arguments are correct 
+    if(argc < 6 || argc > 7)
+    {
+        printf("\nInvalid number of arguments!!!\n");
+        exit(1);
+    }
+    //reading commandline parameters
+    char *baseName = argv[1];
+    char *paramFile = argv[2];
+    char *fileExtFile = argv[3];
+    int kNN = atoi(argv[4]);
+    TSADIST distTsld = atof(argv[5]); 
+    if( argc == 7 ){verbos = atoi(argv[6]);}
+    
+    //read params from the paramFile
+    paramHand.readParamsFromFile(paramFile);
+    myProcParamsPtr = paramHand.getParamPtr();
+    
+    //read file extensions from the file
+    paramHand.readFileExtsInfoFile(fileExtFile);
+    myFileExtsPtr = paramHand.getExtPtr();
+    
+    //create a data handler object
+    TSAdataHandler *TSData1 = new TSAdataHandler(baseName, &logs.procLogs, myFileExtsPtr, myProcParamsPtr);
+    
+    TSData1->genTemplate1SubSeqs();
+    
+    TSAsubSeq_t *subSeqPtr = TSData1->subSeqPtr;
+    
+    
+    TSAdtwSimilarity dtwUCR;
+    
+    int lenMotifReal = TSData1->procParams.motifLengths[TSData1->procParams.indexMotifLenReal];
+    dtwUCR.configureTSASimilarity(lenMotifReal, lenMotifReal, TSData1->procParams.DTWBand);
+    
+    dtwUCR.setQueryPtr(TSData1->subSeqPtr, TSData1->nSubSeqs);
+    dtwUCR.computeQueryEnvelops();
+    dtwUCR.copyQueryEnv2Cand();
+    
+    int nInterFact = TSData1->procParams.nInterpFac;
+    TSADIST LB_kim_FL, LB_Keogh_EQ, LB_Keogh_EC, realDist, bsf=FLT_MAX;
+    TSADATA **U, **L, *accLB1, *accLB2;
+    
+    U = dtwUCR.envUQueryPtr;
+    L = dtwUCR.envLQueryPtr;
+    accLB1 = dtwUCR.accLB_Keogh_EQ;
+    accLB2 = dtwUCR.accLB_Keogh_EC;
+    
+    TSApool *pool = new TSApool(kNN, myProcParamsPtr->blackDur);
+    pool->initPriorityQDisc();
+    
+    for(TSAIND ii=0;ii< TSData1->nSubSeqs;ii++)
+    {
+        for(TSAIND jj=ii+1;jj< TSData1->nSubSeqs;jj++)
+        {
+            if (paramHand.procParams.combMTX[ii%nInterFact][jj%nInterFact]==0)
+                continue;
+            if (fabs(subSeqPtr[ii].sTime-subSeqPtr[jj].sTime)< TSData1->procParams.blackDur)
+            {
+                continue;
+            }
+            realDist = dtw1dBandConst(subSeqPtr[ii].pData, subSeqPtr[jj].pData, lenMotifReal, lenMotifReal, dtwUCR.costMTX, SqEuclidean, dtwUCR.bandDTW, bsf, accLB1);
+            if (realDist <= bsf)
+            {
+                bsf = pool->managePriorityQDisc(subSeqPtr, ii, jj, realDist);
+            }
+        }
+    }
+    
+    
+    TSData1->dumpDiscMotifInfo(TSData1->fHandle.getOutFileName(), pool->priorityQDisc, pool->K, verbos);
+    
+    //generate pattern sub sequences
+    
+    
+    //prepare lower bounding data
+    
+    //enter into nested loop
+    
+    //free memories
+    
+    //dump the generate data
+    
+    delete TSData1;
+    delete pool;
+    
+    if (verbos){printf("Processing done!\n");}
+    
+    return 1;
+}
+
