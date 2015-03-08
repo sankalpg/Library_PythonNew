e2705973003e6988318f31bb02f76b5e83972b48
Sankalp Gulati <sankalp.gulati@upf.edu>
2015-03-08 15:39:43 +0100

    Trivial change

---
+++ networkAnalysis/networkBuild/compileConstructNetwork.sh
@@ -0,0 +1,1 @@
+g++ -I/home/sankalp/Snap-2.2/snap-core -I/home/sankalp/Snap-2.2/glib-core /home/sankalp/Snap-2.2/snap-core/Snap.o constructNetwork.cpp  -O3 -o constructNetwork_O3
--- networkAnalysis/networkBuild/compileConstructNetwork_AMD.sh
+++ networkAnalysis/networkBuild/compileConstructNetwork_AMD.sh
@@ -1,1 +1,1 @@
-g++ -I../../../snap/snap-core -I../../../snap/glib-core ../../../snap/snap-core/Snap.o constructNetwork.cpp  -O3 -o constructNetwork_O3
+g++ -I../../../snap/snap-core -I../../../snap/glib-core ../../../snap/snap-core/Snap.o constructNetwork.cpp  -O3 -o constructNetwork_O3 -lrt
