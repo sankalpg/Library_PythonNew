142f4a32c7700c52ab1a2f9942ca9ffa5a729e48
Sankalp  GULATI <sgulati@deploy>
2015-04-10 15:46:28 +0200

    Small changes in AMD compilation file

--- networkAnalysis/clusteringCoffGenFilteredNetwork_AMD.sh
+++ networkAnalysis/clusteringCoffGenFilteredNetwork_AMD.sh
@@ -1,1 +1,1 @@
-g++ -I../../../snap/snap-core -I../../../snap/glib-core ../../../snap/snap-core/Snap.o clusteringCoffGenFilteredNetwork.cpp  -O3 -o clusteringCoffGenFilteredNetwork_O3 -lrt
+g++ -I../../snap/snap-core -I../../snap/glib-core ../../snap/snap-core/Snap.o clusteringCoffGenFilteredNetwork.cpp  -O3 -o clusteringCoffGenFilteredNetwork_O3 -lrt
--- networkAnalysis/clusteringCoffGen_AMD.sh
+++ networkAnalysis/clusteringCoffGen_AMD.sh
@@ -1,1 +1,1 @@
-g++ -I../../../snap/snap-core -I../../../snap/glib-core ../../../snap/snap-core/Snap.o constructNetwork.cpp  -O3 -o constructNetwork_O3 -lrt
+g++ -I../../snap/snap-core -I../../snap/glib-core ../../snap/snap-core/Snap.o clusteringCoffGen.cpp  -O3 -o clusteringCoffGen_O3 -lrt
