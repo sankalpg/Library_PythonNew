f2dd57fedf91c520c3de0e9b57d3b7706ebe33ec
Sankalp Gulati <sankalp.gulati@upf.edu>
2015-04-10 15:40:47 +0200

    Added file for Clustering coff computation
    These files only analyse clustering coff after thresholding the graph resulted from the filtering

---
+++ networkAnalysis/clusteringCoffGenFilteredNetwork_Intel.sh
@@ -0,0 +1,1 @@
+g++ -I/home/sankalp/Snap-2.2/snap-core -I/home/sankalp/Snap-2.2/glib-core /home/sankalp/Snap-2.2/snap-core/Snap.o clusteringCoffGenFilteredNetwork.cpp  -O3 -o clusteringCoffGenFilteredNetwork_O3
