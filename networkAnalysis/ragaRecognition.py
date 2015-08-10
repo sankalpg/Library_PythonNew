87fbecb6346117a1be15de95f2a383bdbbe74ee0
Sankalp Gulati <sankalp.gulati@upf.edu>
2015-08-10 19:57:43 +0200

    Raga id using document classification technique
    Uses simple tf-idf representation and NB classifier

--- networkAnalysis/ragaRecognition.py
+++ networkAnalysis/ragaRecognition.py
@@ -11,6 +11,11 @@ import time
 import networkProcessing as net_pro
 import communityCharacterization as comm_char
 import json
+from collections import Counter
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
 
 INF = np.finfo(np.float).max
 
@@ -387,9 +392,122 @@ def raga_recognition_V1(fileListFile, thresholdBin, pattDistExt, n_fold = 16, to
     
     return
         
+def get_per_recording_data(comm_data):
+    """
+    This function simply transforms the incoming community data which is in the form:
+    {'comm_index': [u'compId': u'', u'mbid': u'', u'nId': <int>, u'ragaId': u''}]}
+    
+    this is transformed to a data type
+    {'mbid': [(comm_id, nId)]}
+    """
+    
+    per_rec_data = {}
+    for comm_id in comm_data:
+        for node_data in comm_data[comm_id]:
+            if not per_rec_data.has_key(node_data['mbid']):
+                per_rec_data[node_data['mbid']]=[]
+            per_rec_data[node_data['mbid']].append((comm_id, node_data['nId']))
+            
+    return per_rec_data
         
+def raga_recognition_V2(fileListFile, thresholdBin, pattDistExt, n_fold = 16, force_build_network=0):
+    """
+    Raga recognition system using document classification and topic modelling techniques.
+    In this approach we treat phrases of a recording as words (basically cluster id). 
+    """
+    
+    #constructing the network
+    t1 = time.time()
+    wghtd_graph_filename = 'graph_temp'+'_'+str(thresholdBin)
+    
+    if force_build_network or not os.path.isfile(wghtd_graph_filename):
+        cons_net.constructNetwork_Weighted_NetworkX(fileListFile, wghtd_graph_filename , thresholdBin, pattDistExt, 0 , -1)
+    
+    full_net = nx.read_pajek(wghtd_graph_filename)
+    
+    #performing community detection on the built network
+    comm_filename = 'comm_thresholdBin'+'_'+str(thresholdBin)+ pattDistExt +'.community'
+    net_pro.detectCommunitiesInNewtworkNX(wghtd_graph_filename, comm_filename)
+    
+    #fetching relevant data for the community
+    comm_data = json.load(open(comm_filename,'r'))
+    comm_char.fetch_phrase_attributes(comm_data, database = myDatabase, user= myUser)
+    
+    #getting per document (recording) words (community index)
+    per_rec_data = get_per_recording_data(comm_data)
+
+    t2 = time.time()
+    print "time taken = %f"%(t2-t1)
+    
+    ##########Loop for N_Fold cross validataion##############
+    raga_mbid = get_mbids_raagaIds_for_collection(myDatabase, myUser)
+    raga_list = [r[0] for r in raga_mbid]
+    mbid_list = [r[1] for r in raga_mbid]
+    raga_map, map_raga = generate_raga_mapping(raga_list)
+    label_list = np.array([raga_map[r] for r in raga_list])
+    
+    #initializing crossfold object
+    cval = cross_val.StratifiedKFold(label_list, n_folds=n_fold)
+    
+    #splitting folds.
+    fold_cnt = 1
+    predicted_raga = ['' for r in range(len(raga_mbid))]
+    
+    count_vect = CountVectorizer()
+    tfidf_transformer = TfidfTransformer()
+    for train_inds, test_inds in cval:
+        docs_train = []
+        for train_ind in train_inds:
+            #if per_rec_data.has_key(mbid_list[test_ind]):
+                #print mbid_list[test_ind], len(per_rec_data[mbid_list[test_ind]])
+                #per_rec_words = [p[0] for p in per_rec_data[mbid_list[test_ind]]]
+                #print per_rec_words
+            #else:
+                #print mbid_list[test_ind], 0
+            if per_rec_data.has_key(mbid_list[train_ind]):
+                per_rec_words = ' '.join([p[0] for p in per_rec_data[mbid_list[train_ind]]])
+            else:
+                per_rec_words = ''
+            docs_train.append(per_rec_words)
         
+        X_train_counts = count_vect.fit_transform(docs_train)
+        X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+        #clf = MultinomialNB().fit(X_train_tfidf, label_list[train_inds])
+        clf = SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=5, random_state=42).fit(X_train_tfidf, label_list[train_inds])
         
+        docs_test = []
+        for test_ind in test_inds:
+            #if per_rec_data.has_key(mbid_list[test_ind]):
+                #print mbid_list[test_ind], len(per_rec_data[mbid_list[test_ind]])
+                #per_rec_words = [p[0] for p in per_rec_data[mbid_list[test_ind]]]
+                #print per_rec_words
+            #else:
+                #print mbid_list[test_ind], 0
+            if per_rec_data.has_key(mbid_list[test_ind]):
+                per_rec_words = ' '.join([p[0] for p in per_rec_data[mbid_list[test_ind]]])
+            else:
+                per_rec_words = ''
+            docs_test.append(per_rec_words)
+        
+        X_test_counts = count_vect.transform(docs_test)
+        X_test_tfidf = tfidf_transformer.transform(X_test_counts)
+        
+        predicted = clf.predict(X_test_tfidf)
+        for ii, pred_val in enumerate(predicted):
+            predicted_raga[test_inds[ii]] = map_raga[pred_val]
+        
+            
+    cnt = 0
+    for i in range(len(predicted_raga)):
+        if raga_list[i] == predicted_raga[i]:
+            cnt+=1
+    
+    print "You got %d number of ragas right for a total of %d number of recordings"%(cnt, len(predicted_raga))
+       
+    
+    return
+    
+    
         
         
         
